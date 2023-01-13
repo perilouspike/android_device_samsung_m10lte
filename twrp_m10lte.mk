6c3d5bcd939b85ba70337f9b3241383237b50fdc
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-01-13 10:05:24 +0530

    Update twrp_m10lte.mk
    Added embedded.mk and removes base.mk

--- twrp_m10lte.mk
+++ twrp_m10lte.mk
@@ -21,7 +21,8 @@ $(call inherit-product, device/samsung/m10lte/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
