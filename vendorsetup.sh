853d668c1e1cf3af6518c902740501124f84a33a
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-01-13 15:06:12 +0530

    Update vendorsetup.sh
    Added OFOX support.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,118 @@
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 TeamWin Recovery Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2019-2021 The OrangeFox Recovery Project
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
 #
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="m10lte"
+#set -o xtrace
 
-add_lunch_combo twrp_m10lte-eng
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	
+	# Initial Exports
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export LC_ALL="C"
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+  
+  # Maintaining Info
+  export FOX_R11="1"
+	export OF_MAINTAINER="perilouspike"
+	export FOX_VERSION="R11.1"
+	export FOX_BUILD_TYPE="Unofficial-Beta"
+	
+	# m10lte specific
+	export TARGET_DEVICE_ALT="m10lte"
+	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY"
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/platform/13540000.dwmmc0/by-name/SYSTEM"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/platform/13540000.dwmmc0/by-name/VENDOR"
+	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/13540000.dwmmc0/by-name/BOOT"
+	export OF_FLASHLIGHT_ENABLE="1"
+	export OF_FL_PATH1="/sys/class/camera/flash"
+	export OF_DEVICE_WITHOUT_PERSIST=1
+	export OF_SKIP_FBE_DECRYPTION=1
+	
+	# Screen Settings
+	export OF_SCREEN_H=2280
+	#export OF_STATUS_H=81
+	#export OF_STATUS_INDENT_LEFT=55 #m20lte Specific
+	#export OF_STATUS_INDENT_RIGHT=55 # m20lte Specific
+	export OF_CLOCK_POS=1
+	export OF_ALLOW_DISABLE_NAVBAR=0
+
+  export PLATFORM_SECURITY_PATCH="2099-12-31"
+  export PLATFORM_VERSION="16.1.0"
+	export FOX_ADVANCED_SECURITY="1"
+	export OF_USE_TWRP_SAR_DETECT="1"
+	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+	export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
+
+	# Quick Backup Defaults
+	export OF_QUICK_BACKUP_LIST="/boot;/data;/system;/vendor;"
+	
+	# Common
+	export TARGET_ARCH=arm64
+	export TW_DEFAULT_LANGUAGE="en"
+	export OF_KEEP_FORCED_ENCRYPTION="1"
+	#export OF_PATCH_AVB20="1"
+	export OF_USE_MAGISKBOOT="1"
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+	export FOX_USE_LZMA_COMPRESSION="1"
+	
+	export FOX_USE_BASH_SHELL="1"
+	export FOX_ASH_IS_BASH="1"
+	export FOX_USE_TAR_BINARY="1"
+	export FOX_USE_ZIP_BINARY="1"
+	export FOX_REPLACE_BUSYBOX_PS="1"
+	export OF_USE_NEW_MAGISKBOOT="1"
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+	
+	# Use the System (ROM) Fingerprint where Available
+	export OF_USE_SYSTEM_FINGERPRINT="1"
+
+	# Run Post Format Process for MTP
+	export OF_RUN_POST_FORMAT_PROCESS=1
+	
+	# Create Odin Flashable .tar files
+	export OF_NO_SAMSUNG_SPECIAL=0
+	
+	# Let's See what are our Build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+fi
+
+#
