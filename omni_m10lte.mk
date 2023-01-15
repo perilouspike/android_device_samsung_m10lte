7902d198b7e84de77116ca2a9c11fe09e0a715c9
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-01-16 01:49:06 +0530

    Update omni_m10lte.mk

--- omni_m10lte.mk
+++ omni_m10lte.mk
@@ -18,9 +18,9 @@
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from the Device Tree itself.
 $(call inherit-product, device/samsung/m10lte/device.mk)
