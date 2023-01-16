1db73de8496670ffe861dd6529acc82b97619aa6
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-01-17 02:25:27 +0530

    Update omni_m10lte.mk
    Massive cleanup

--- omni_m10lte.mk
+++ omni_m10lte.mk
@@ -12,24 +12,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Inherit from the Device Tree itself.
-$(call inherit-product, device/samsung/m10lte/device.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Inherit from Omni-common Stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
+# Inherit from the Device Tree itself.
+$(call inherit-product, device/samsung/m10lte/device.mk)
 
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/dt.img:boot.img
 
 # Device identifier. This must come after all inclusions
+PRODUCT_RELEASE_NAME := m10lte
 PRODUCT_DEVICE := m10lte
 PRODUCT_NAME := omni_m10lte
 PRODUCT_BRAND := samsung
