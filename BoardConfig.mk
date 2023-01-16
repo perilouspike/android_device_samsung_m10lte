85203896c334c21fc576a8cec94252a0d6899631
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-01-17 02:18:57 +0530

    Update BoardConfig.mk
    Massive cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,9 +1,18 @@
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 TeamWin Recovery Project
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# SPDX-License-Identifier: Apache-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 DEVICE_PATH := device/samsung/m10lte
 
@@ -18,6 +27,7 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
+# 2ND Architecture
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
@@ -25,53 +35,141 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
+# APEX
+OVERRIDE_TARGET_FLATTEN_APEX := true
+
 TARGET_USES_64_BIT_BINDER := true
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := m10lte,m10ltecis,m10ltexx,m10lteub,m10lteins,m10ltedtvvj,m10ltekx,m10ltedx,m10ltedd,m10f
+
 # Bootloader
-BOARD_VENDOR := samsung
-TARGET_BOARD_PLATFORM := exynos5
-TARGET_SOC := exynos7870
-TARGET_BOOTLOADER_BOARD_NAME := universal7870
+TARGET_SOC := $(TARGET_BOOTLOADER_BOARD_NAME)
+TARGET_BOOTLOADER_BOARD_NAME := exynos7870
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
+
+# Platform
+BOARD_VENDOR := samsung
+PRODUCT_PLATFORM :=universal7870
+TARGET_BOARD_PLATFORM := $(PRODUCT_PLATFORM)
 
 # Display
 TARGET_SCREEN_DENSITY := 320
 
-# Kernel
-BOARD_KERNEL_BASE := 0x10000000
+# Custom bootimg
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/dtbhtool/mkbootimg.mk
+
+# Kernel - Info
+TARGET_KERNEL_ARCH := $(TARGET_ARCH)
+TARGET_KERNEL_HEADER_ARCH := $(TARGET_ARCH)
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_PREBUILT_KERNEL := device/samsung/m10lte/prebuilt/kernel
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/m10lte/prebuilt/dt.img
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_KERNEL_SECOND_OFFSET := 0x00f00000
+BOARD_KERNEL_IMAGE_NAME := Image
+#BOARD_KERNEL_SEPARATED_DT := true
+#BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive androidboot.selinux=permissive
+
+# Args
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
+BOARD_MKBOOTIMG_ARGS += --dt device/samsung/m10lte/prebuilt/dt.img
+
+# Kernel Source
+TARGET_KERNEL_CONFIG := m10lte_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/m10lte
+TARGET_KERNEL_CLANG_COMPILE := true
+LC_ALL="C"
+
+# Kernel - prebuilt
+TARGET_FORCE_PREBUILT_KERNEL := true
+ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_DT := $(DEVICE_PATH)/prebuilt/dt.img
+BOARD_KERNEL_SEPARATED_DT := 
+endif
 
 # Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 39845888
+BOARD_CACHEIMAGE_PARTITION_SIZE := 92274688
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2566914048
+BOARD_VENDORIMAGE_PARTITION_SIZE := 251658240
 
-# Platform
-TARGET_BOARD_PLATFORM := universal7870
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+
+# Workaround to fix Vendor Copying error
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_CRYPTO_SAMSUNG := true
+TW_INCLUDE_FBE := true
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
+LZMA_RAMDISK_TARGETS := recovery # LZMA support
+TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
+
+# System as root
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# Android Verified Boot
+BOARD_AVB_ENABLE := false
+BOARD_BUILD_DISABLED_VBMETAIMAGE := true
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 PLATFORM_VERSION := 16.1.0
+PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
-LZMA_RAMDISK_TARGETS := recovery
+TW_INCLUDE_NTFS_3G := true
+TW_INCLUDE_FASTBOOTD := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
 
-# Additional flags
-TARGET_OTA_ASSERT_DEVICE := m10lte,m10ltecis,m10ltexx,m10lteub,m10lteins,m10ltedtvvj,m10ltekx,m10ltedd,m10f
-BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/bootimg.mk
-TARGET_NO_RADIOIMAGE := true
+# Maintainer/Version
+TW_DEVICE_VERSION := perilouspike
+
+# Set brightness path and level
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 1000
+TW_DEFAULT_BRIGHTNESS := 500
+
+# Use Toolbox instead of Busybox
+TW_USE_TOOLBOX := true
+
+# Debug-tools
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+
+# exFAT/NTFS file system Support
 TW_INCLUDE_FUSE_EXFAT := true
 TW_INCLUDE_FUSE_NTFS := true
+
+#
