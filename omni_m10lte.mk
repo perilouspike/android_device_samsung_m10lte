0c4b3cf03191e6c8f7fda1fa2a476acddb8034cb
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-01-20 04:21:27 +0530

    Update omni_m10lte.mk
    Added aosp_base_telephony.mk

--- omni_m10lte.mk
+++ omni_m10lte.mk
@@ -14,11 +14,11 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Otherwise, If you have 32-bit device, add the below line instead of above line
