642d956127cd9e816da8a40e14de9d9e8bacc59a
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-01-20 00:24:40 +0530

    Update omni_m10lte.mk
    Reverted some changes

--- omni_m10lte.mk
+++ omni_m10lte.mk
@@ -14,10 +14,10 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
@@ -41,9 +41,9 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M105F
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+#PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="m10ltedd-user 10 QP1A.190711.020 M105FDDS4CVG1 release-keys"
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+    #PRIVATE_BUILD_DESC="m10ltedd-user 10 QP1A.190711.020 M105FDDS4CVG1 release-keys"
 
-BUILD_FINGERPRINT := samsung/m10ltedd/m10lte:10/QP1A.190711.020/M105FDDS4CVG1:user/release-keys
+#BUILD_FINGERPRINT := samsung/m10ltedd/m10lte:10/QP1A.190711.020/M105FDDS4CVG1:user/release-keys
